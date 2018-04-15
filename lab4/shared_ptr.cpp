6122235486ae809b7cfb8bfd329a1bda03079654
Denmey <Denmey98@gmail.com>
2018-04-15 13:00:06 +0300

    Step 2 and report done

--- lab4/shared_ptr.cpp
+++ lab4/shared_ptr.cpp
@@ -6,214 +6,444 @@
 
 namespace stepik
 {
-  template <typename T>
-  class shared_ptr
-  {
-  public:
-    explicit shared_ptr(T *ptr = nullptr)
-    {
-	 m_ptr = ptr;
-	 m_count = nullptr;
-	 if (m_ptr)
-	 {
-		 #ifdef DEBUG
-		 	std::cout << "Constructing new shared " << this << " from ptr " << ptr << std::endl;
-		 #endif
-		 m_count = new long(1);
-		 if (!m_count) throw std::bad_alloc{};
-	 }
-    }
+template <typename T>
+class shared_ptr
+{
+public:
+	template<class Y>
+	friend class shared_ptr;
 
-    ~shared_ptr() noexcept
-    {
-	 #ifdef DEBUG
-	 	std::cout << "Destroying shared_ptr " << this << std::endl;
-	 #endif
-	 if (m_ptr)
-	 {
-		 --(*m_count);
-		 if ((*m_count) == 0)
-		 {
-			#ifdef DEBUG
-				std::cout << "Counter is zero, deleting owned object " << m_ptr << std::endl;
-			#endif
-		 	delete m_ptr;
-			delete m_count;
+	explicit shared_ptr(T *ptr = nullptr)
+	{
+		#ifdef DEBUG
+			std::cout << "Constructing shared " << this << " from ptr "
+					<< ptr << std::endl;
+		#endif
+		m_ptr = ptr;
+		m_count = nullptr;
+		if (m_ptr)
+		{
+			m_count = new long(1);
+			if (!m_count) throw std::bad_alloc{};
 		}
-	 }
-    }
+	}
 
-    shared_ptr(const shared_ptr & other) noexcept
-    {
-	 #ifdef DEBUG
-	 	std::cout << "Constructing shared_ptr " << this << " from shared_ptr " << &other << std::endl;
-	 #endif
-	 m_ptr = other.m_ptr;
-	 m_count = other.m_count;
-	 if (m_ptr)
-		 ++(*m_count);
-    }
+	~shared_ptr() noexcept
+	{
+		#ifdef DEBUG
+			std::cout << "Destroying shared_ptr " << this << std::endl;
+		#endif
+		if (m_ptr)
+		{
+			--(*m_count);
+			if ((*m_count) == 0)
+			{
+				#ifdef DEBUG
+					std::cout << "Counter is zero, deleting owned object "
+							<< m_ptr << std::endl;
+				#endif
+				delete m_ptr;
+				delete m_count;
+			}
+		}
+	}
 
-    shared_ptr& operator=(const shared_ptr & other) noexcept
-    {
-	 #ifdef DEBUG
-	 	std::cout << "Assigning shared ptrs: " << this << " = " << &other << std::endl;
-	 #endif
-	 if (this == &other) return *this;
-	 if (m_ptr)
-	 {
-		 --(*m_count);
-		 if((*m_count)==0)
-		 {
-			#ifdef DEBUG
- 				std::cout << "Counter is zero, deleting owned object " << m_ptr << std::endl;
- 			#endif
-		 	delete m_ptr;
-			delete m_count;
-			m_ptr = nullptr;
-			m_count = nullptr;
-		 }
-	 }
-	 m_ptr = other.m_ptr;
-	 m_count = other.m_count;
-	 if (m_ptr)
-		 ++(*m_count);
-	 return *this;
-    }
+	shared_ptr(const shared_ptr & other) noexcept
+	{
+		#ifdef DEBUG
+			std::cout << "Constructing shared_ptr " << this << " from shared_ptr "
+					<< &other << std::endl;
+		#endif
+		m_ptr = other.m_ptr;
+		m_count = other.m_count;
+		if (m_ptr)
+		++(*m_count);
+	}
 
-    explicit operator bool() const noexcept
-    {
-     #ifdef DEBUG2
-     std::cout << "Bool called " << this << std::endl;
-     #endif
-	 if (m_ptr) return true;
-	 return false;
-    }
+	template<class Y>
+	shared_ptr(const shared_ptr<Y> & other) noexcept
+	{
+		#ifdef DEBUG
+			std::cout << "Constructing shared_ptr " << this << " from shared_ptr "
+					<< &other << std::endl;
+		#endif
+		m_ptr = dynamic_cast<T*>(other.m_ptr);
+		m_count = other.m_count;
+		if (m_ptr)
+		++(*m_count);
+	}
 
-    T* get() const noexcept
-    {
-     #ifdef DEBUG2
-     std::cout << "Get called " << this << std::endl;
-     #endif
-	 return m_ptr;
-    }
+	shared_ptr& operator=(const shared_ptr &other) noexcept
+	{
+		#ifdef DEBUG
+		std::cout << "Assigning shared ptrs: " << this << " = "
+		<< &other << std::endl;
+		#endif
+		if ((void*)this == (void*)&other) return *this;
+		if (m_ptr)
+		{
+			--(*m_count);
+			if((*m_count)==0)
+			{
+				#ifdef DEBUG
+					std::cout << "Counter is zero, deleting owned object "
+							<< m_ptr << std::endl;
+				#endif
+				delete m_ptr;
+				delete m_count;
+				m_ptr = nullptr;
+				m_count = nullptr;
+			}
+		}
+		m_ptr = other.m_ptr;
+		m_count = other.m_count;
+		if (m_ptr)
+		++(*m_count);
+		return *this;
+	}
 
-    long use_count() const noexcept
-    {
-     #ifdef DEBUG2
-     std::cout << "Count called " << this << std::endl;
-     #endif
-	 if (!m_count) return 0;
-	 return *m_count;
-    }
+	template<class Y>
+	shared_ptr& operator=(const shared_ptr<Y> &other) noexcept
+	{
+		#ifdef DEBUG
+			std::cout << "Assigning shared ptrs: " << this << " = "
+					<< &other << std::endl;
+		#endif
+		if ((void*)this == (void*)&other) return *this;
+		if (m_ptr)
+		{
+			--(*m_count);
+			if((*m_count)==0)
+			{
+				#ifdef DEBUG
+					std::cout << "Counter is zero, deleting owned object "
+							<< m_ptr << std::endl;
+				#endif
+				delete m_ptr;
+				delete m_count;
+				m_ptr = nullptr;
+				m_count = nullptr;
+			}
+		}
+		m_ptr = dynamic_cast<T*>(other.m_ptr);
+		m_count = other.m_count;
+		if (m_ptr)
+		++(*m_count);
+		return *this;
+	}
 
-    T& operator*() const noexcept
-    {
-     #ifdef DEBUG2
-     std::cout << "* called " << this << std::endl;
-     #endif
-	 return *m_ptr;
-    }
+	explicit operator bool() const noexcept
+	{
+		#ifdef DEBUG2
+			std::cout << "Bool called " << this << std::endl;
+		#endif
+		if (m_ptr) return true;
+		return false;
+	}
 
-    T* operator->() const noexcept
-    {
-     #ifdef DEBUG2
-     std::cout << "-> called " << this << std::endl;
-     #endif
-	 return m_ptr;
-    }
+	T* get() const noexcept
+	{
+		#ifdef DEBUG2
+			std::cout << "Get called " << this << std::endl;
+		#endif
+		return m_ptr;
+	}
 
-    void swap(shared_ptr& x) noexcept
-    {
-	 #ifdef DEBUG2
-	 	std::cout << "Swapping shared ptrs " << this << " and " << &x << std::endl;
-	 #endif
-	 T *tmp_ptr = m_ptr;
-	 long *tmp_count = m_count;
-	 m_ptr = x.m_ptr;
-	 m_count = x.m_count;
-	 x.m_ptr = tmp_ptr;
-	 x.m_count = tmp_count;
-    }
+	long use_count() const noexcept
+	{
+		#ifdef DEBUG2
+			std::cout << "Count called " << this << std::endl;
+		#endif
+		if (!m_count) return 0;
+		return *m_count;
+	}
 
-    void reset(T *ptr = nullptr)
-    {
-	 #ifdef DEBUG
-	 	std::cout << "Resetting shared_ptr " << this << " from " << m_ptr << " to " << ptr << std::endl;
-	 #endif
-	 if (m_ptr)
-	 {
+	T& operator*() const noexcept
+	{
+		#ifdef DEBUG2
+			std::cout << "* called " << this << std::endl;
+		#endif
+		return *m_ptr;
+	}
+
+	T* operator->() const noexcept
+	{
+		#ifdef DEBUG2
+		std::cout << "-> called " << this << std::endl;
+		#endif
+		return m_ptr;
+	}
+
+	void swap(shared_ptr& x) noexcept
+	{
+		#ifdef DEBUG2
+			std::cout << "Swapping shared ptrs " << this << " and "
+			<< &x << std::endl;
+		#endif
+		T *tmp_ptr = m_ptr;
+		long *tmp_count = m_count;
+		m_ptr = x.m_ptr;
+		m_count = x.m_count;
+		x.m_ptr = tmp_ptr;
+		x.m_count = tmp_count;
+	}
+
+	void reset(T *ptr = nullptr)
+	{
+		#ifdef DEBUG
+			std::cout << "Resetting shared_ptr " << this << " from " << m_ptr
+				<< " to " << ptr << std::endl;
+		#endif
+		if (m_ptr)
+		{
 		 --(*m_count);
 		 if ((*m_count)==0)
 		 {
 			#ifdef DEBUG
-				std::cout << "Counter is zero, deleting owned object " << m_ptr << std::endl;
+				std::cout << "Counter is zero, deleting owned object "
+						<< m_ptr << std::endl;
 			#endif
 			delete m_ptr;
 			delete m_count;
 			m_ptr = nullptr;
 			m_count = nullptr;
 		}
-	 }
-	 m_ptr = ptr;
-	 m_count = nullptr;
-	 if (m_ptr)
-	 {
-	 	m_count = new long(1);
-		if (!m_count) throw std::bad_alloc{};
-	 }
-    }
+		}
+		m_ptr = ptr;
+		m_count = nullptr;
+		if (m_ptr)
+		{
+			m_count = new long(1);
+			if (!m_count) throw std::bad_alloc{};
+		}
+	}
 
-  private:
-    // data members
-    T *m_ptr;
-    long *m_count;
-  };
+private:
+	// data members
+	T *m_ptr;
+	long *m_count;
+};
+
+	// Comparison operators
+		template <class T, class U>
+		bool operator==(const shared_ptr<T> &lhs,
+							 const shared_ptr<U> &rhs) noexcept
+		{
+			return (lhs.get() == rhs.get());
+		}
+
+		template <class T, class U>
+		bool operator!=(const shared_ptr<T> &lhs,
+							 const shared_ptr<U> &rhs) noexcept
+		{
+			return !(lhs == rhs);
+		}
+
+		template <class T, class U>
+		bool operator<(const shared_ptr<T> &lhs,
+							 const shared_ptr<U> &rhs) noexcept
+		{
+			return (lhs.get() < rhs.get());
+		}
+
+		template <class T, class U>
+		bool operator>(const shared_ptr<T> &lhs,
+							 const shared_ptr<U> &rhs) noexcept
+		{
+			return (rhs < lhs);
+		}
+
+		template <class T, class U>
+		bool operator<=(const shared_ptr<T> &lhs,
+							 const shared_ptr<U> &rhs) noexcept
+		{
+			return !(rhs < lhs);
+		}
+
+		template <class T, class U>
+		bool operator>=(const shared_ptr<T> &lhs,
+							 const shared_ptr<U> &rhs) noexcept
+		{
+			return !(lhs < rhs);
+		}
+
+		// Class and nullptr
+		template <class T>
+		bool operator==(const shared_ptr<T> &lhs, std::nullptr_t rhs) noexcept
+		{
+			return !lhs;
+		}
+		template <class T>
+		bool operator==(std::nullptr_t lhs, const shared_ptr<T> &rhs) noexcept
+		{
+			return !rhs;
+		}
+		template <class T>
+		bool operator!=(const shared_ptr<T> &lhs, std::nullptr_t rhs) noexcept
+		{
+			return (bool)lhs;
+		}
+		template <class T>
+		bool operator!=(std::nullptr_t lhs, const shared_ptr<T> &rhs) noexcept
+		{
+			return (bool)rhs;
+		}
+		template <class T>
+		bool operator<(const shared_ptr<T> &lhs, std::nullptr_t rhs) noexcept
+		{
+			return lhs.get() < nullptr;
+		}
+		template <class T>
+		bool operator<(std::nullptr_t lhs, const shared_ptr<T> &rhs) noexcept
+		{
+			return nullptr < rhs.get();
+		}
+		template <class T>
+		bool operator>(const shared_ptr<T> &lhs, std::nullptr_t rhs) noexcept
+		{
+			return nullptr < lhs;
+		}
+		template <class T>
+		bool operator>(std::nullptr_t lhs, const shared_ptr<T> &rhs) noexcept
+		{
+			return rhs < nullptr;
+		}
+		template <class T>
+		bool operator<=(const shared_ptr<T> &lhs, std::nullptr_t rhs) noexcept
+		{
+			return !(nullptr < lhs);
+		}
+		template <class T>
+		bool operator<=(std::nullptr_t lhs, const shared_ptr<T> &rhs) noexcept
+		{
+			return !(rhs < nullptr);
+		}
+		template <class T>
+		bool operator>=(const shared_ptr<T> &lhs, std::nullptr_t rhs) noexcept
+		{
+			return !(lhs < nullptr);
+		}
+		template <class T>
+		bool operator>=(std::nullptr_t lhs, const shared_ptr<T> &rhs) noexcept
+		{
+			return !(nullptr < rhs);
+		}
+	// End of comparison operators
+
 } // namespace stepik
+
+class Base
+{
+public:
+	virtual ~Base() = default;
+	virtual void print()
+	{
+		std::cout << "1" << std::endl;
+	}
+};
+
+class Derived : public Base
+{
+public:
+	virtual void print() override
+	{
+		std::cout << "2" << std::endl;
+	}
+};
+
 
 int main()
 {
+	{
 	std::cout << "Step1--------------------" << std::endl;
 	int* p = new int(3);
 	{
 		std::cout << "Pointer: " << p << " " << *p << std::endl;
-		stepik::shared_ptr<int>* a = new stepik::shared_ptr<int>(p);
+		stepik::shared_ptr<int> a{p};
 
-		std::cout << "Shared ptr initialized by pointer: " << a->get() << std::endl
-				<< "Value: " << **a << std::endl
-				<< "Count: " << a->use_count() << std::endl
-				<< "Bool: " << bool(*a) << std::endl;
-		stepik::shared_ptr<int> b{*a};
+		std::cout << "Shared ptr initialized by pointer: "
+				<< a.get() << std::endl
+				<< "Value: " << *a << std::endl
+				<< "Count: " << a.use_count() << std::endl
+				<< "Bool: " << bool(a) << std::endl;
+		stepik::shared_ptr<int> b{a};
 		stepik::shared_ptr<int> c;
-		std::cout << "Shared ptr initialized by another shared: " << b.get() << std::endl
+
+		std::cout << std::endl;
+		std::cout << "Shared ptr initialized by another shared: "
+				<< b.get() << std::endl
 				<< "Value: " << *b << std::endl
 				<< "Count: " << b.use_count() << std::endl
 				<< "Bool: " << bool(b) << std::endl;
+		std::cout << std::endl;
 		std::cout << "Empty shared ptr: " << c.get() << std::endl
 				<< "Count: " << c.use_count() << std::endl
 				<< "Bool: " << bool(c) << std::endl;
-		c = *a;
-		std::cout << "After assigning shrd ptr first to empty: " << c.get() << std::endl
+
+		std::cout << std::endl;
+		c = a;
+		std::cout << "After assigning first shrd ptr to empty: "
+				<< c.get() << std::endl
 				<< "Value: " << *c << std::endl
 				<< "Count: " << c.use_count() << std::endl
 				<< "Bool: " << bool(c) << std::endl;
+
+		std::cout << std::endl;
 		c.reset();
-		std::cout << "After resetting shared to nullptr: " << c.get() << std::endl
+		std::cout << "After resetting shared to nullptr: "
+				<< c.get() << std::endl
 				// << "Value: " << *c << std::endl
 				<< "Count: " << c.use_count() << std::endl
 				<< "Bool: " << bool(c) << std::endl;
-		a->swap(c);
+
+		std::cout << std::endl;
+		a.swap(c);
 		std::cout << "After swapping shared ptrs: " << c.get() << std::endl
 				<< "Value: " << *c << std::endl
 				<< "Count: " << c.use_count() << std::endl
 				<< "Bool: " << bool(c) << std::endl;
-		// c.~shared_ptr();
-		std::cout << "After destroying shared ptr: " << c.get() << std::endl
-				// << "Value: " << *c << std::endl
-				<< "Count: " << c.use_count() << std::endl
-				<< "Bool: " << bool(c) << std::endl;
-
 	}
-	std::cout << "Shared ptrs destroyed, pointer: " << p << " " << *p << std::endl;
+	std::cout << "Shared ptrs destroyed, pointer: " << p << " "
+			<< *p << std::endl;
+	}
+
+	std::cout << std::endl << "Step2--------------------" << std::endl;
+	stepik::shared_ptr<Derived> derivedPtr(new Derived);
+	stepik::shared_ptr<Base> basePtr = derivedPtr, basePtr2(new Base);
+	std::cout << "Printing from basePtr that pointing at base: " << std::endl;
+	basePtr2->print();
+	std::cout << "Printing from basePtr that pointing at derived: " << std::endl;
+	basePtr->print();
+	std::cout << basePtr.get() << " " << derivedPtr.get() << ":" << std::endl;
+	std::cout << "'==': " << (basePtr==derivedPtr) << std::endl;
+	std::cout << "'!=': " << (basePtr!=derivedPtr) << std::endl;
+	std::cout << "'<': " << (basePtr<derivedPtr) << std::endl;
+	std::cout << "'>': " << (basePtr>derivedPtr) << std::endl;
+	std::cout << "'<=': " << (basePtr<=derivedPtr) << std::endl;
+	std::cout << "'>=': " << (basePtr>=derivedPtr) << std::endl;
+
+	std::cout << basePtr.get() << " " << basePtr2.get() << ":" << std::endl;
+	std::cout << "'==': " << (basePtr==basePtr2) << std::endl;
+	std::cout << "'!=': " << (basePtr!=basePtr2) << std::endl;
+	std::cout << "'<': " << (basePtr<basePtr2) << std::endl;
+	std::cout << "'>': " << (basePtr>basePtr2) << std::endl;
+	std::cout << "'<=': " << (basePtr<=basePtr2) << std::endl;
+	std::cout << "'>=': " << (basePtr>=basePtr2) << std::endl;
+
+	std::cout << "nullptr " << basePtr.get() << std::endl;
+	std::cout << "'==': " << (nullptr==basePtr) << std::endl;
+	std::cout << "'!=': " << (nullptr!=basePtr) << std::endl;
+	std::cout << "'<': " << (nullptr<basePtr) << std::endl;
+	std::cout << "'>': " << (nullptr>basePtr) << std::endl;
+	std::cout << "'<=': " << (nullptr<=basePtr) << std::endl;
+	std::cout << "'>=': " << (nullptr>=basePtr) << std::endl;
+
+	std::cout << basePtr.get() << " nullptr" << std::endl;
+	std::cout << "'==': " << (basePtr==nullptr) << std::endl;
+	std::cout << "'!=': " << (basePtr!=nullptr) << std::endl;
+	std::cout << "'<': " << (basePtr<nullptr) << std::endl;
+	std::cout << "'>': " << (basePtr>nullptr) << std::endl;
+	std::cout << "'<=': " << (basePtr<=nullptr) << std::endl;
+	std::cout << "'>=': " << (basePtr>=nullptr) << std::endl;
 	return 0;
 }
